700289106fff9ed8413135ac3bbe9909b83c1b45
GSM352366D01LE\Emmanuel <emmanuel.quiroz@elektra.com.mx>
2022-12-06 17:59:52 -0600

    Add proyect

---
+++ app/src/main/java/mx/com/upax/huella/huellas_android_sdk_mascota/MascotasActivity.kt
@@ -0,0 +1,11 @@
+package mx.com.upax.huella.huellas_android_sdk_mascota
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MascotasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
